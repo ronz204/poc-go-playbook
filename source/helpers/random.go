eae7ce6ee918f8257cb9a98abd8f9b773c1fa42a
roney ruiz <roneyruizrojas@gmail.com>
2026-02-17 21:09:04 -0600

    feat: add random flow method to random

--- source/helpers/random.go
+++ source/helpers/random.go
@@ -5,3 +5,7 @@ import "math/rand/v2"
 func RandomInt(min, max int) int {
 	return rand.IntN(max-min) + min
 }
+
+func RandomFloat(min, max float64) float64 {
+	return rand.Float64()*(max-min) + min
+}
